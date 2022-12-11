e7e0987cc3e2fa33bc99be1db67fc6ff49bd5325
Kahelaraw <ismaelplangca@gmail.com>
2022-12-11 03:52:25 +0000

    Added Day 5

--- Day_05/Day.cs
+++ Day_05/Day.cs
@@ -1,32 +1,70 @@
+using System.Text.RegularExpressions;
 namespace Day_05
 {
     public class Day
     {
         string[] input;
         Stack<char>[] stacks;
+        Regex regex;
         public Day()
         {
             input = File.ReadAllText("Day_05\\input.txt").Split("\n\n");
             stacks = ParseStacks(input[0]);
+            regex = new Regex(@"^move (\d+) from (\d+) to (\d+)$");
         }
         public void S1()
         {
-
+            foreach(var line in input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries) )
+                ExecuteS1(ParseMove(line) );
+            var output = String.Join("", stacks.Select(a => a.Peek().ToString() ) );
+            Console.WriteLine(output);
+        }
+        public void S2()
+        {
+            foreach(var line in input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries) )
+                ExecuteS2(ParseMove(line) );
+            var output = String.Join("", stacks.Select(a => a.Peek().ToString() ) );
+            Console.WriteLine(output);
         }
         Stack<char>[] ParseStacks(string input)
         {
-            var stacks = new Stack<char>[9];
-            foreach(var line in input.Split("\n") )
+            var stacks = Enumerable.Repeat(0, 9)
+                .Select(a => new Stack<char>() )
+                .ToArray();
+            foreach(var line in input.Split("\n").Reverse().Skip(1) )
             {
                 var count = 0;
                 for(int i = 1; i < line.Length; i += 4)
                 {
-                    if(line[i] != '\0')
+                    if(line[i] != ' ')
                         stacks[count].Push(line[i]);
                     count++;
                 }
             }
             return stacks;
+        }
+        int[] ParseMove(string line)
+        {
+            var groups = regex.Match(line).Groups;
+            var count  = Int32.Parse(groups[1].Value);
+            var source = Int32.Parse(groups[2].Value) - 1;
+            var dest   = Int32.Parse(groups[3].Value) - 1;
+
+            return new int[]{count, source, dest};
+        }
+        void ExecuteS1(int[] move)
+        {
+            for(int i = 0; i < move[0]; i++)
+                stacks[move[1]].Push(stacks[move[2]].Pop() );
+        }
+        void ExecuteS2(int[] move)
+        {
+            var temp = new List<char>();
+            for(int i = 0; i < move[0]; i++)
+                temp.Add(stacks[move[1]].Pop() );
+            temp.Reverse();
+            for(int i = 0; i < move[0]; i++)
+                stacks[move[2]].Push(temp[i]);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿public class Program
 {
+    // Days -> Day01.S1
+    // Inputs -> Day1.txt
     public static void Main(string[] args)
     {
         var sol = new Day_05.Day();
-        sol.S1();
+        sol.S2();
     }
 }
