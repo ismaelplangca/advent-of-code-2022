3887f2d2cf032c8084e47cc67b5b6ef8a2970ce4
Kahelaraw <ismaelplangca@gmail.com>
2022-12-18 20:52:18 +0000

    refactoring

--- Day_01/Day.cs
+++ Day_01/Day.cs
@@ -7,16 +7,15 @@ namespace Day_01
         public Day()
         {
             input = File.ReadAllLines("Day_01\\input.txt").ToList();
-            elves = new();
-            var count = 0;
-            foreach(var line in input)
-            {
-                if(line.Equals("") ) {
-                    elves.Add(count);
-                    count = 0;
-                } else
-                    count += int.Parse(line);
-            }
+            elves = input
+                .Aggregate(new List<int>() { 0 }, (list, line) => {
+                        if(String.IsNullOrWhiteSpace(line) ) {
+                            list.Add(0);
+                            return list;
+                        }
+                        list[list.Count() - 1] += int.Parse(line);
+                        return list;
+                    });
         }
         public int S1()
         {
